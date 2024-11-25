6cfc2edb84904f50cb57525c87a3019f66c99537
Steephan2004 <131771084+Steephan2004@users.noreply.github.com>
2024-11-25 20:32:10 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
-        <h1 className='head-text'>Get In Touch</h1>
+        <h1 className='head-text'>Get In Touch!!!</h1>
         <form className='w-full flex flex-col gap-7 mt-14' onSubmit={handleSubmit}>
           <label className='text-black-500 font-semibold'>
             Name
